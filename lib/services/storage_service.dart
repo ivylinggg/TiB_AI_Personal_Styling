77b05d2209a6bf26d106a156e235570e18ac8422
ivylinggg <tp076996@mail.apu.edu.my>
2026-08-13 18:33:34 +0800

    Add wardrobe image storage

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -5,24 +5,25 @@ import 'package:firebase_storage/firebase_storage.dart';
 class StorageService {
   StorageService._();
 
-  static final FirebaseStorage _storage =
-      FirebaseStorage.instance;
+  static final FirebaseStorage _storage = FirebaseStorage.instance;
 
   static Future<String> uploadAnalysisImage({
     required String uid,
     required File image,
   }) async {
-    final fileName =
-        DateTime.now().millisecondsSinceEpoch.toString();
-
-    final ref = _storage
-        .ref()
-        .child("analysis")
-        .child(uid)
-        .child("$fileName.jpg");
-
+    final fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    final ref = _storage.ref().child('analysis').child(uid).child('$fileName.jpg');
     await ref.putFile(image);
+    return ref.getDownloadURL();
+  }
 
-    return await ref.getDownloadURL();
+  static Future<String> uploadWardrobeImage({
+    required String uid,
+    required File image,
+  }) async {
+    final fileName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
+    final ref = _storage.ref().child('wardrobe').child(uid).child(fileName);
+    await ref.putFile(image);
+    return ref.getDownloadURL();
   }
 }
