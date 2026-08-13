c161ca1ba4c9c07c70848316bca15ad43562878e
ivylinggg <tp076996@mail.apu.edu.my>
2026-08-13 18:06:16 +0800

    Fix AnalysisProvider registration

--- lib/app.dart
+++ lib/app.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'core/theme/light_theme.dart';
-
+import 'providers/analysis_provider.dart';
 import 'providers/auth_provider.dart';
-
 import 'screens/splash/splash_screen.dart';
 
 class TibApp extends StatelessWidget {
@@ -12,15 +11,19 @@ class TibApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AuthProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+          create: (_) => AuthProvider(),
+        ),
+        ChangeNotifierProvider<AnalysisProvider>(
+          create: (_) => AnalysisProvider(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-
         title: 'TiB AI',
-
         theme: LightTheme.theme,
-
         home: const SplashScreen(),
       ),
     );
