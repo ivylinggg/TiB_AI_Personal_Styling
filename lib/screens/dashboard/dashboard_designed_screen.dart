ed739d18fdbb052ded88bc9aa97f5d3f4f1be6df
ivylinggg <tp076996@mail.apu.edu.my>
2026-08-21 16:55:48 +0800

    Restore dashboard to valid Flutter screen

--- lib/screens/dashboard/dashboard_designed_screen.dart
+++ lib/screens/dashboard/dashboard_designed_screen.dart
@@ -1,1 +1,10 @@
-PLACEHOLDER
+import 'package:flutter/material.dart';
+
+class DashboardDesignedScreen extends StatelessWidget {
+  const DashboardDesignedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("Dashboard")));
+  }
+}
