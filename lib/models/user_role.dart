7441b87427e8cac41b7f1036cfc73291fd7780e6
ivylinggg <tp076996@mail.apu.edu.my>
2026-08-28 14:02:13 +0800

    feat: add consultant user role

--- lib/models/user_role.dart
+++ lib/models/user_role.dart
@@ -1,11 +1,12 @@
-enum UserRole { customer, admin }
+enum UserRole { customer, consultant, admin }
 
 extension UserRoleExtension on UserRole {
   String get value {
     switch (this) {
       case UserRole.customer:
         return 'customer';
-
+      case UserRole.consultant:
+        return 'consultant';
       case UserRole.admin:
         return 'admin';
     }
@@ -15,7 +16,9 @@ extension UserRoleExtension on UserRole {
     switch (value?.toLowerCase()) {
       case 'admin':
         return UserRole.admin;
-
+      case 'consultant':
+      case 'staff':
+        return UserRole.consultant;
       case 'customer':
       default:
         return UserRole.customer;
