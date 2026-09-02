bf769b83d82bb991470b70319c75d973a5ee6e9e
ivylinggg <tp076996@mail.apu.edu.my>
2026-09-02 11:12:45 +0800

    feat: add outfit check domain models

---
+++ lib/models/outfit_check_models.dart
@@ -0,0 +1,11 @@
+class OutfitItemSuggestion {
+  final String category;
+  final String title;
+  final String reason;
+
+  const OutfitItemSuggestion({
+    required this.category,
+    required this.title,
+    required this.reason,
+  });
+}
