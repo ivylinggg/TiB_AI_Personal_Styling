7d217ca5d717451ef39505a8985366bdc594d24d
ivylinggg <tp076996@mail.apu.edu.my>
2026-09-03 18:33:20 +0800

    Fix generated widget test after Flutter project regeneration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tib_ai_personal_styling/app.dart';
 
 void main() {
   testWidgets('TiB app smoke test', (WidgetTester tester) async {
-    expect(true, isTrue);
+    expect(find.byType(TibApp), findsNothing);
   });
 }
