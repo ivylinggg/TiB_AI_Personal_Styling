77df1f14d4695691e696db4d436199401ba8d77b
ivylinggg <tp076996@mail.apu.edu.my>
2026-09-09 10:59:54 +0800

    refactor: harden auth session state

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,15 +1,51 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 
 class AuthProvider extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  User? get user => _auth.currentUser;
+  StreamSubscription<User?>? _subscription;
+  User? _user;
+  bool _initialized = false;
+  bool _busy = false;
 
-  bool get isLoggedIn => user != null;
+  AuthProvider() {
+    _user = _auth.currentUser;
+    _subscription = _auth.authStateChanges().listen((user) {
+      final changed = _user?.uid != user?.uid;
+      _user = user;
+      _initialized = true;
+      if (changed) notifyListeners();
+    });
+  }
+
+  User? get user => _user;
+  bool get isLoggedIn => _user != null;
+  bool get isInitialized => _initialized;
+  bool get isBusy => _busy;
+
+  String? get uid => _user?.uid;
+  String? get displayName => _user?.displayName;
+  String? get email => _user?.email;
+  String? get photoUrl => _user?.photoURL;
 
   Future<void> signOut() async {
-    await _auth.signOut();
+    if (_busy) return;
+    _busy = true;
     notifyListeners();
+    try {
+      await _auth.signOut();
+    } finally {
+      _busy = false;
+      notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
   }
 }
