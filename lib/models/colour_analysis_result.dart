73ea009cd85babe18a18b5e38300548d6d87b382
ivylinggg <tp076996@mail.apu.edu.my>
2026-09-09 12:18:31 +0800

    Expand colour analysis into trait-first personal colour profile

--- lib/models/colour_analysis_result.dart
+++ lib/models/colour_analysis_result.dart
@@ -10,6 +10,11 @@ class ColourAnalysisResult {
   final Map<String, double> faceMeasurements;
   final List<String> faceStylingGuidance;
   final List<String> colourReasons;
+  final String chroma;
+  final String clarity;
+  final List<String> bestNeutrals;
+  final List<String> accentColours;
+  final List<String> lessIdealColours;
 
   const ColourAnalysisResult({
     required this.season,
@@ -23,5 +28,10 @@ class ColourAnalysisResult {
     this.faceMeasurements = const {},
     this.faceStylingGuidance = const [],
     this.colourReasons = const [],
+    this.chroma = 'Unknown',
+    this.clarity = 'Unknown',
+    this.bestNeutrals = const [],
+    this.accentColours = const [],
+    this.lessIdealColours = const [],
   });
 }
