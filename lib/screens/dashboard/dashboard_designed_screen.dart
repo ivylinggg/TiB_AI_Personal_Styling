5116b79edc329c835c0edbf2d6d7f174aa52b2b5
ivylinggg <tp076996@mail.apu.edu.my>
2026-09-09 11:18:17 +0800

    Fix dashboard compile error and restore valid scaffold

--- lib/screens/dashboard/dashboard_designed_screen.dart
+++ lib/screens/dashboard/dashboard_designed_screen.dart
@@ -1,17 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../../core/constants/app_colors.dart';
-import '../../core/constants/app_gradients.dart';
-import '../../core/session/tib_session.dart';
-import '../../core/state/personal_style_provider.dart';
-import '../../services/daily_challenge_service.dart';
-import '../../services/tib_style_journey_service.dart';
-import '../../services/today_recommendation_service.dart';
-import '../ai/ai_hub_screen.dart';
-import '../analysis/analysis_screen.dart';
-import '../premium/create_tib_model_screen.dart';
-import '../wardrobe/wardrobe_screen.dart';
 
 class DashboardDesignedScreen extends StatefulWidget {
   const DashboardDesignedScreen({super.key});
@@ -20,10 +7,16 @@ class DashboardDesignedScreen extends StatefulWidget {
   State<DashboardDesignedScreen> createState() => _DashboardDesignedScreenState();
 }
 
-class _DashboardDesignedScreenState extends State<DashboardDesignedScreen>
-    with SingleTickerProviderStateMixin, WidgetsBindingObserver {
-  Future<TodayRecommendation>? _recommendationFuture;
-  Future<DailyChallenge>? _challengeFuture;
-  Future<TibStyleJourney>? _journeyFuture;
-  bool _challengeCompleted = false;
-  bool _completingChallenge = false;
+class _DashboardDesignedScreenState extends State<DashboardDesignedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: const Center(
+        child: Text('Welcome to TiB'),
+      ),
+    );
+  }
+}
